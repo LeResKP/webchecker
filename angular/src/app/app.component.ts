e92a8f4e6c949b584ac331e00f90ac3f9a6db603
Aurélien Matouillot <a.matouillot@gmail.com>
2018-12-10 18:46:00 +0100

    Add search in urls

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+
+import {fromEvent, Subscription} from 'rxjs';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
+
 
 import { STATUS } from './constants';
 import { UrlService } from './url.service';
@@ -10,18 +14,31 @@ import { UrlService } from './url.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnDestroy, OnInit {
   title = 'webchecker';
   urls = <any>[];
+  _urls = <any>[];
   STATUS = STATUS;
+
+  keyupSub:  Subscription;
+  @ViewChild('input') inputElRef: ElementRef;
 
   constructor(private router: Router, private urlService: UrlService) {}
 
   ngOnInit() {
     this.urlService.getUrls().subscribe((urls) => {
       this.urls = urls;
-      // TODO: redirect when there is no id defined
-      // this.router.navigate(['/urls', this.urls[0].url_id]);
+      this._urls = urls;
     });
+    this.keyupSub = fromEvent(this.inputElRef.nativeElement, 'keyup').pipe(
+      debounceTime(100),
+      distinctUntilChanged(),
+    ).subscribe((event: KeyboardEvent) => {
+      this.urls = this._urls.filter(v => v.url.indexOf((<HTMLInputElement>event.target).value) > -1);
+    });
+  }
+
+  ngOnDestroy() {
+    this.keyupSub.unsubscribe();
   }
 }
