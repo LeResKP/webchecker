035b9c4e88fb0fb9b9379e0f9a2d05623e1d1b7f
Aurélien Matouillot <a.matouillot@gmail.com>
2019-02-26 08:16:25 +0100

    Improve urls

--- angular/src/app/action/action.component.ts
+++ angular/src/app/action/action.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ProjectService } from '../project.service';
+
 
 @Component({
   selector: 'app-action',
   templateUrl: './action.component.html',
   styleUrls: ['./action.component.css']
 })
-export class ActionComponent implements OnInit {
+export class ActionComponent implements OnDestroy, OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.projectService.setCurrentAction(this.route.snapshot.data.action);
+  }
+
+  ngOnDestroy() {
+    this.projectService.resetCurrentAction();
   }
 
 }
